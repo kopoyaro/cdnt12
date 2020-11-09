44b17e3efb0d072a79129817bea9270d178874f5
kopo <platevoetlaurent@hotmail.com>
2020-11-09 19:54:37 +0100

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SwitchToggleComponent } from './switch-toggle/switch-toggle.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SwitchToggleComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
